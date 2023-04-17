7004f97a393557111699e06a571fc1546193120f
Deepika Singh <deepika.singh@dcthomson.co.uk>
2023-04-17 17:44:25 +0100

    give any userdefined commit message

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -7,29 +7,27 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/feature/homepage.feature",
+        //glue=  {"com.example.StepDefinition"},
+        plugin = {"pretty:STDOUT", "html:target/cucumber-html-report.html", "json:target/cucumber-reports/cucumber.json", "junit:target/cucumber.xml", "rerun:target/rerun.txt"},
+        //tags="@login-page",
+        dryRun = false
+
+)
+public class Runner {
+
+}
+
 //@RunWith(Cucumber.class)
 //@CucumberOptions(
-//        features = "src/test/feature/homepage.feature",
-//        //glue=  {"com.example.StepDefinition"},
-//        plugin = {"pretty:STDOUT", "html:target/cucumber-html-report.html", "json:target/cucumber.json", "junit:target/cucumber.xml", "rerun:target/rerun.txt"},
-//        //tags="@login-page",
-//        dryRun = false
-//
+//        features ={"src/test/resources/features"},
+//        //glue = {"com.example.StepDefinition"},
+//        plugin = {"pretty:STDOUT",
+//                "json:target/cucumber.json",
+//                "html:target/cucumber-html-report.html",
+//        }
 //)
 //public class Runner {
-//
 //}
-
-public class Runner {
-    // add this method to generate the JUnit test result file
-    @AfterClass
-    public static void generateJUnitResult() {
-        File file = new File("target/cucumber.xml");
-        List<String> args = Arrays.asList(
-                "--plugin", "junit:" + file.getAbsolutePath(),
-                "--glue", "com.example.StepDefinition",
-                "src/test/feature/homepage.feature"
-        );
-        io.cucumber.core.cli.Main.run(args.toArray(new String[0]), Thread.currentThread().getContextClassLoader());
-    }
-}
