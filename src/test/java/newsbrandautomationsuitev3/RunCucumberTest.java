26ad9afa6933f4fcc493d1880141e25a2777ea9c
Deepika Singh <deepika.singh@dcthomson.co.uk>
2023-05-05 17:03:22 +0100

    new files

--- src/test/java/newsbrandautomationsuitev3/RunCucumberTest.java
+++ src/test/java/newsbrandautomationsuitev3/RunCucumberTest.java
@@ -1,15 +1,15 @@
 package newsbrandautomationsuitev3;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("newsbrandautomationsuitev3")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+//@IncludeTags("Home-page-menu")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value="junit:target/cucumber-reports/Cucumber.xml, json:target/cucumber-reports/Cucumber.json, html:target/cucumber-reports/Cucumber.html, timeline:target/cucumber-reports/CucumberTimeline")
 public class RunCucumberTest {
 }
